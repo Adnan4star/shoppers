0fac55b7eaa743e69acba847c4cff032f99e9dab
DESKTOP-97AQQUP\dani <adnanafzal684@gmail.com>
2024-03-01 07:04:44 +0500

    Backend categories list limits to 2 per page

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -22,7 +22,7 @@ class CategoryController extends Controller
             $categories = $categories->where('name','like','%'.$request->get('keyword').'%'); //search by keyword on list category
         }
 
-        $categories = $categories->paginate(4); 
+        $categories = $categories->paginate(2); 
         return view('admin.category.list',compact('categories')); //displaying categories
     }
 
